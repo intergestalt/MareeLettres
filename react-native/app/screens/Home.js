7e588b99ea8982adb60d9357c6c80586f299e3dc
retani <holgheiss@gmail.com>
2017-07-12 18:14:12 +0200

    NUIT-4 Hello World! #close

--- react-native/app/screens/Home.js
+++ react-native/app/screens/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
+import { HomeIntro } from '../components/HomeIntro';
 
 export default () => (
     <Container>
         <StatusBar />
-        <View />
+        <HomeIntro />
     </Container>
 );
