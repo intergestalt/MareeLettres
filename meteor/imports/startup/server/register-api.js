28cea7861a25878640d475694aeee6db30a1b51d
retani <holgheiss@gmail.com>
2017-07-28 16:51:05 +0200

    server: remove locations collection

--- meteor/imports/startup/server/register-api.js
+++ meteor/imports/startup/server/register-api.js
@@ -1,8 +1,6 @@
 // Register your apis here
 
-import '../../api/locations/methods';
-import '../../api/locations/server/publications';
-
+import '../../api/challenges/methods';
 import '../../api/challenges/server/publications';
 
 import '../../api/proposals/server/publications';
