ac745529aa88d5af8a7aa51f6110d917726d5657
retani <holgheiss@gmail.com>
2017-08-05 09:47:32 +0200

    server: Can only call _ensureIndex on server collections

--- meteor/imports/api/proposals/proposals.js
+++ meteor/imports/api/proposals/proposals.js
@@ -1,8 +1,11 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 export const Proposals = new Mongo.Collection('proposals');
 
-Proposals._ensureIndex({ challenge_id: 1 });
+if (Meteor.isServer) {
+  Proposals._ensureIndex({ challenge_id: 1 });
+}
 
 Proposals.allow({
   insert: () => false,
