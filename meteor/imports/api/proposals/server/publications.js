42efe1bffbe590dd4da06e40c6ad283dcedd1067
retani <holgheiss@gmail.com>
2017-08-08 21:40:40 +0200

    server: refactor: use named parameters in REST URLs

--- meteor/imports/api/proposals/server/publications.js
+++ meteor/imports/api/proposals/server/publications.js
@@ -2,21 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import { Proposals } from '../proposals';
 import { Challenges } from '../../challenges/challenges';
 
-Meteor.publish(
-  'get.proposals/challenge_id/limit',
-  function getProposals(challenge_id, limit) {
-    const query = {
-      challenge_id,
-    };
-    const options = {
-      limit: parseInt(limit),
-    };
-    return Proposals.find(query, options);
-  },
-  {
-    url: `${Meteor.settings.public.api_prefix}challenges/:0/proposals/limit/:1`,
-  },
-);
+Meteor.publish('get.proposals/challenge_id/limit', function getProposals(challenge_id, limit) {
+  const query = {
+    challenge_id,
+  };
+  const options = {
+    limit: parseInt(limit),
+  };
+  return Proposals.find(query, options);
+});
 
 Meteor.publish('get.proposals/challenge_id', function getProposals(challenge_id) {
   const query = challenge_id ? { challenge_id } : {};
@@ -35,33 +29,30 @@ JsonRoutes.add('get', `${Meteor.settings.public.api_prefix}proposals`, function 
   });
 });
 
-JsonRoutes.add('get', `${Meteor.settings.public.api_prefix}challenges/:0/proposals`, function (
-  req,
-  res,
-  next,
-) {
-  const challenge_id = req.params[0];
-  const limit = parseInt(req.query.limit) || 0; // ?limit=:limit
+JsonRoutes.add(
+  'get',
+  `${Meteor.settings.public.api_prefix}challenges/:challenge_id/proposals`,
+  function (req, res, next) {
+    const challenge_id = req.params.challenge_id;
+    const limit = parseInt(req.query.limit) || 0; // ?limit=:limit
 
-  const proposals = Proposals.find({ challenge_id }, { limit }).fetch();
+    const proposals = Proposals.find({ challenge_id }, { limit }).fetch();
 
-  const options = {};
+    const options = {};
 
-  if (proposals.length === 0 && Challenges.find({ _id: challenge_id }).count() === 0) {
-    const error = new Meteor.Error(
-      'challenge-not-found',
-      `Challenge ${challenge_id} not found`,
-    );
-    error.statusCode = 404;
-    throw error;
-  } else {
-    options.data = {
-      proposals,
-    };
-  }
+    if (proposals.length === 0 && Challenges.find({ _id: challenge_id }).count() === 0) {
+      const error = new Meteor.Error('challenge-not-found', `Challenge ${challenge_id} not found`);
+      error.statusCode = 404;
+      throw error;
+    } else {
+      options.data = {
+        proposals,
+      };
+    }
 
-  JsonRoutes.sendResult(res, options);
-});
+    JsonRoutes.sendResult(res, options);
+  },
+);
 
 JsonRoutes.add(
   // deprecated
