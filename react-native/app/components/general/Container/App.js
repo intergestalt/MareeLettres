7f3483d144a6319d4313205a3526861740aebd3b
retani <holgheiss@gmail.com>
2017-08-11 07:32:45 +0200

    NUIT-16 add impact font. new wrapper componen AppContainer wraps whole app and provides font. use impact font in top menu. update versions to expo 19.0.1 and react native 0.46.1 (minor update, but yields several other package updates in yarn.lock.

---
+++ react-native/app/components/general/Container/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Font } from 'expo';
+
+import styles from './styles';
+
+class AppContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      impact: require('./assets/fonts/impact.ttf'),
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <Text>Loading...</Text>;
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+export default AppContainer;
