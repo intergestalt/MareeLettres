a49d1390aceaa36ed4d779572dc8d471dee3196e
Xavier Burrow <jxburrow@gmail.com>
2017-08-19 15:28:47 +0200

    prep

--- react-native/app/config/initialState.js
+++ react-native/app/config/initialState.js
@@ -1,15 +1,12 @@
 import { OriginId } from 'maree-lettres-shared';
 import { PROPOSAL_LIST_MODES, CHALLENGE_VIEWS, PROPOSAL_VIEWS } from '../consts';
 
-const sampleDate = (new Date()).toUTCString();
+const sampleDate = (new Date()).getTime();
 const lat = 52.4975;
 const lng = 13.4377;
 
 export default {
   user: {
-    // NOTE: Return string is very long. Results in complex (ugly) QR Codes.
-    //       Rework generateFromDeviceId? A set-length hash function might work
-    //       -xavier
     origin_id: OriginId.generateFromDeviceId(Expo.Constants.deviceId),
     primary_letter: {
       character: 'X',
@@ -31,7 +28,7 @@ export default {
       },{
         character: 'D',
         acquired_at: sampleDate,
-        last_used_at: sampleDate, 
+        last_used_at: sampleDate,
       },
     ],
     votes: [{
