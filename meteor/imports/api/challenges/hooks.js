85a9ff82c03d2c1b0e3198653a4c95cc73808b3c
retani <holgheiss@gmail.com>
2017-08-22 14:35:09 +0200

    server: remove debug output

--- meteor/imports/api/challenges/hooks.js
+++ meteor/imports/api/challenges/hooks.js
@@ -5,5 +5,4 @@ console.log('attach challenges hooks');
 
 Challenges.before.insert(function (userId, doc) {
   doc.proposals_amount = Proposals.find({ challenge_id: doc._id }).count();
-  console.log(doc);
 });
