88d01f875e71e9d17afd36cf950ab2b768b0788d
Per Violet <violet@punktwunder.de>
2017-09-08 15:17:38 +0200

    correction

--- react-native/app/components/general/Container/Screen.js
+++ react-native/app/components/general/Container/Screen.js
@@ -3,8 +3,12 @@ import { View, BackAndroid } from 'react-native';
 import { connect } from 'react-redux';
 
 import styles from './styles';
-import { CHALLENGE_VIEWS, SCREENS } from '../../../consts';
-import { popProposalSubmitter, popChallengeSelector } from '../../../helper/navigationProxy';
+import { CHALLENGE_VIEWS, SCREENS, MAP_VIEWS } from '../../../consts';
+import {
+  dispatchBackActionToMapOverview,
+  popProposalSubmitter,
+  popChallengeSelector,
+} from '../../../helper/navigationProxy';
 
 class Screen extends Component {
   static propTypes = {
@@ -14,6 +18,7 @@ class Screen extends Component {
     centerContent: PropTypes.bool,
     screen: PropTypes.string,
     challengeView: PropTypes.string,
+    mapView: PropTypes.string,
   };
   constructor(props) {
     super(props);
@@ -32,6 +37,18 @@ class Screen extends Component {
         return true;
       } else if (this.props.challengeView === CHALLENGE_VIEWS.DETAIL) {
         popChallengeSelector(this.props);
+        return true;
+      }
+    }
+    console.log(`${this.props.screen} ${this.props.mapView}`);
+    if (this.props.screen === SCREENS.MAP) {
+      if (
+        this.props.mapView === MAP_VIEWS.CAMERA ||
+        this.props.mapView === MAP_VIEWS.LETTER_SELECTOR ||
+        this.props.mapView === MAP_VIEWS.QR_CODE_GET ||
+        this.props.mapView === MAP_VIEWS.QR_CODE_SEND
+      ) {
+        dispatchBackActionToMapOverview(this.props, MAP_VIEWS.OVERVIEW);
         return true;
       }
     }
@@ -59,9 +76,11 @@ const mapStateToProps = (state) => {
   try {
     const screen = state.globals.screen;
     const challengeView = state.challenges.challengeView;
+    const mapView = state.globals.mapView;
     return {
       screen,
       challengeView,
+      mapView,
     };
   } catch (e) {
     console.log('Screen');
