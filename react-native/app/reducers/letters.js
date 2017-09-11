19e449260b13f51b656bd1bd2448133fde7f61f5
Sebastian Quack <sebastianquack@gmail.com>
2017-09-11 14:57:59 +0200

    load max_map_markers correctly from config

--- react-native/app/reducers/letters.js
+++ react-native/app/reducers/letters.js
@@ -50,7 +50,8 @@ const letters = (state = initialState.letters, action) => {
         }
 
         const currentRegion = store.getState().user.map.coordinates;
-        const markerLimit = store.getState().config.map_max_markers; // hard limit on markers from server
+        const markerLimit = store.getState().config.config.map_max_markers; // hard limit on markers from server
+        console.log("map_max_markers: " + markerLimit);
         let counter = 0;
         let droppedMarkers = 0;
         
