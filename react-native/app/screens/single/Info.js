49663e4f1c7fb0f419bc4a8a858bca6c393d1cbd
Holger Heißmeyer <holgheiss@gmail.com>
2017-09-11 14:22:26 +0200

    move reset button to info section

--- react-native/app/screens/single/Info.js
+++ react-native/app/screens/single/Info.js
@@ -1,17 +1,28 @@
 import React, { PropTypes, Component } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Button } from 'react-native';
 
 import { Screen } from '../../components/general/Container';
 import { InfoBox } from '../../components/info/InfoBox';
+import { deleteAllFromStorage } from '../../helper/localStorage';
 
 class Info extends Component {
   static propTypes = {
     navigation: PropTypes.object,
   };
+
+  handleDeleteStorage() {
+    console.log("delete storage")
+    deleteAllFromStorage();
+  }
+
   render() {
     return (
-      <Screen navigation={this.props.navigation} backgroundColor="#aaaa00">
+      <Screen navigation={this.props.navigation}>
         <StatusBar />
+        <Button
+          onPress={this.handleDeleteStorage}
+          title="Reset App and delete all personal data"
+        />
         <InfoBox />
       </Screen>
     );
