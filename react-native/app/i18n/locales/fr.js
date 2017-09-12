06a421ffca51d9ad62d39bc15770e4ed8bfff540
Holger Heißmeyer <holgheiss@gmail.com>
2017-09-12 13:09:12 +0200

    shorten ticker string

--- react-native/app/i18n/locales/fr.js
+++ react-native/app/i18n/locales/fr.js
@@ -7,7 +7,7 @@ export default {
   top_menu_option_4: 'INFO',
 
   challenge: 'THEME',
-  live_indicator: 'vote en direct',
+  live_indicator: 'en direct',
 
   proposal_list_header_most: 'Top',
   proposal_list_header_newest: 'Dernier',
@@ -52,14 +52,14 @@ export default {
   map_scan_qr_instruction: 'Scannez les flashcodes de vos amis pour obtenir leurs lettres.',
   map_tutorial_1_title: 'Welcome to the FLUX!',
   map_tutorial_1_text: 'To choose YOUR letter, tap on the + symbol right under You.',
-  map_tutorial_3_title: 'Excellent work!', 
+  map_tutorial_3_title: 'Excellent work!',
   map_tutorial_3_text: 'Want to write with different letters? Get letters from your friends by scanning their QR code. Tap the Get Letters below.',
   center_map: 'Centre map',
   camera_button: 'Camera',
   choose_your_letter: 'Quelle lettre souhaitez-vous être? Choisissez bien, vous ne pourrez changer de lettre pendant 24h.',
-  map_too_zoom_title: 'Too high in the sky', 
+  map_too_zoom_title: 'Too high in the sky',
   map_too_zoom_text: 'Zoom in to the circle place letters!',
-  map_too_crowded_title: 'Too crowded', 
+  map_too_crowded_title: 'Too crowded',
   map_too_crowded_text: "There are too many letters in this area. Please write somewhere else or wait.",
   map_too_far_title: 'Too far away',
   map_too_far_text: "You cannot write outside the circle around you.",
@@ -67,7 +67,7 @@ export default {
   map_photo_save_text: "Find your pics in your device's camera roll!",
 
   info_about_title: 'NUIT BLANCHE FR',
-  
+
   network_error: 'Network error. Please check your internet connection. FR',
 
 };
