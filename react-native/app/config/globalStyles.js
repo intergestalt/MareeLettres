b46064dde7ace40574bc864c6433aee33b2ef236
Holger Heißmeyer <holgheiss@gmail.com>
2017-09-13 01:05:29 +0200

    comments

--- react-native/app/config/globalStyles.js
+++ react-native/app/config/globalStyles.js
@@ -7,7 +7,7 @@ const globalStyles = {
     $strokeWidth: screenHeight > 800 ? 3 : screenHeight > 600 ? 3 : 2,
     $proposalPaddingHorizontal: '10%',
     outline: 0, // set to 1 to see the elements boundaries
-    rem: screenHeight > 800 ? 24 : screenHeight > 600 ? 20 : 16,
+    rem: screenHeight > 800 ? 24 : screenHeight > 600 ? 20 : 16, // dividable by 4 -> 0.25rem, 0.5rem, 0.75rem
 }
 
 export default globalStyles
