018ec131ebe8d70808ca0219b28e7e822ed44059
Holger Heißmeyer <holgheiss@gmail.com>
2017-09-13 17:00:48 +0200

    change stroke width

--- react-native/app/config/globalStyles.js
+++ react-native/app/config/globalStyles.js
@@ -5,7 +5,7 @@ const globalStyles = {
     $backgroundColorMenuItem: 'rgb(245,132,102)',
     $highlightDraggingLetterColor: 'rgb(245,132,102)',
     $draggingLetterCursorColor: 'rgb(90, 88, 112)',
-    $strokeWidth: screenHeight > 800 ? 3 : screenHeight > 600 ? 3 : 2,
+    $strokeWidth: 1,
     $proposalPaddingHorizontal: '10%',
     outline: 0, // set to 1 to see the elements boundaries
     rem: screenHeight > 800 ? 24 : screenHeight > 600 ? 20 : 16, // dividable by 4 -> 0.25rem, 0.5rem, 0.75rem
