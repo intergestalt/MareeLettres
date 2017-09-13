d8707796c7b2d4c8c9b9109fab12e48123ea2c34
Per Violet <violet@punktwunder.de>
2017-09-13 20:00:42 +0200

    Debugging

--- react-native/app/i18n/locales/en.js
+++ react-native/app/i18n/locales/en.js
@@ -10,7 +10,8 @@ export default {
   live_indicator: 'live',
 
   vote_tutorial_1_title: 'Welcome',
-  vote_tutorial_1_text: 'Below, you can see a list of topics that will be decided by vote. Tap one!',
+  vote_tutorial_1_text:
+    'Below, you can see a list of topics that will be decided by vote. Tap one!',
   vote_tutorial_2_title: 'Time to swipe!',
   vote_tutorial_2_text: 'To vote on suggestions for this topic, swipe left or right.',
 
@@ -66,17 +67,20 @@ export default {
   camera_button: 'Camera',
   take_picture: 'Take picture EN',
 
-  proposal_blocked: '!\n\n EN We are sorry, but your Submission for VOTE#{NUM} does not comply with our community guidelines. It may be o ensive or inappropriate. Please try with another answer.',
+  proposal_blocked:
+    '!\n\n EN We are sorry, but your Submission for VOTE#{NUM} does not comply with our community guidelines. It may be o ensive or inappropriate. Please try with another answer.',
   proposal_in_review1: 'EN Thank you for your submission for VOTE#{NUM}',
   proposal_in_review2: 'Status: in review EN',
   proposal_passed: 'EN Your submission for VOTE#{NUM} is currently at',
   proposal_rank: 'EN Rank',
   try_again_button: 'Try Again EN',
-  choose_your_letter: 'Which letter do you want to be? Choose wisely, you cannot change this for the next 24 hours.',
+  choose_your_letter:
+    'Which letter do you want to be? Choose wisely, you cannot change this for the next 24 hours.',
   map_too_zoom_title: 'Too high in the sky',
   map_too_zoom_text: 'Zoom in to the circle place letters!',
   map_too_crowded_title: 'Too crowded',
-  map_too_crowded_text: 'There are too many letters in this area. Please write somewhere else or wait.',
+  map_too_crowded_text:
+    'There are too many letters in this area. Please write somewhere else or wait.',
   map_too_far_title: 'Too far away',
   map_too_far_text: 'You cannot write outside the circle around you.',
   map_photo_save_title: 'Saved',
@@ -85,4 +89,7 @@ export default {
   network_error: 'Network error. Please check your internet connection. EN',
   suggestion_too_late_title: 'EN Too late!',
   suggestion_too_late_text: 'EN Suggestions closed already. Sorry <3',
+  suggestion_empty_title: 'EN !',
+  suggestion_empty_text: 'EN You must at least drag one letter.',
+  suggestion_wrong: 'EN Oups. Something did not work. Sorry!',
 };
