1c26d515d0330c7ab9b686405dbc060b5c658082
Sebastian Quack <sebastianquack@gmail.com>
2017-09-19 23:04:59 +0200

    add basic news and map to website

---
+++ web/maree-web/src/components/Map/Map.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+} from "react-google-maps";
+import mapStyles from "./google-maps-style.json";
+
+const FluxMap = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100%`, width: `100%`}} />,
+    mapElement: <div style={{ height: `100%` }} />,
+    center: { lat: 25.03, lng: 121.6 },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={16}
+    defaultCenter={{lat: 48.856788, lng: 2.3447006}}
+    defaultOptions={{ styles: mapStyles, mapTypeControl: false, zoomControl: false, fullscreenControl: false, streetViewControl: false }}
+  >
+  </GoogleMap>
+);
+
+export default FluxMap;
--- web/maree-web/src/components/general/Container/App.js
+++ web/maree-web/src/components/general/Container/App.js
@@ -3,6 +3,8 @@ import { translate } from 'react-i18next';
 import InfoColumn from '../../InfoColumn/InfoColumn';
 import LanguageSelector from '../LanguageSelector/LanguageSelector';
 import ChallengeList from '../../ChallengeList/ChallengeList';
+import NewsFeed from '../../NewsFeed/NewsFeed';
+import FluxMap from '../../Map/Map';
 import './App.css';
 
 class App extends React.Component {
@@ -22,9 +24,11 @@ class App extends React.Component {
         </section>
         <section className="news">
           <div className="sectionHeader">NEWS</div>
+          <NewsFeed/>
         </section>
         <section className="flux">
           <div className="sectionHeader">FLUX</div>
+          <FluxMap/>
         </section>
       </div>
     );
