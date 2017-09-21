84825d432d0cffd790d6068ae689de2eb9ae1a60
Holger Heißmeyer <holgheiss@gmail.com>
2017-09-21 20:00:02 +0200

    remove dependencies, put db indexes in extra file

--- meteor/imports/api/proposals/schema.js
+++ meteor/imports/api/proposals/schema.js
@@ -3,28 +3,23 @@ import SimpleSchema from 'simpl-schema';
 const ProposalsSchemaObject = {
     origin_ids: {
         type: Array,
-        index: 1,
     },
     challenge_id: {
         type: String,
-        index: 1,
     },
     text: {
         type: String,
     },
     score: {
         type: SimpleSchema.Integer,
-        index: 1, // TODO: check direction
         defaultValue: 0,
     },
     votes_amount: {
         type: SimpleSchema.Integer,
-        index: 1, // TODO: check direction
         defaultValue: 0,
     },
     score_trending: {
         type: Number,
-        index: 1, // TODO: check direction
         defaultValue: 0,
     },
     yes_votes: {
@@ -54,18 +49,15 @@ const ProposalsSchemaObject = {
 const TinderProposalsSchemaObject = {
     origin_ids: {
         type: Array,
-        index: 1,
     },
     challenge_id: {
         type: String,
-        index: 1,
     },
     text: {
         type: String,
     },
     tinderscore: {
         type: SimpleSchema.Integer,
-        index: 1, // TODO: check direction
     },
 }
 
