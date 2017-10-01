bfe92d4508ce23ce5b75d4a3b3d9938ca4429f65
Sebastian Quack <sebastianquack@gmail.com>
2017-10-01 12:14:40 +0200

    correct letter base measurments

--- meteor/imports/ui/screens/Spacer.jsx
+++ meteor/imports/ui/screens/Spacer.jsx
@@ -13,40 +13,40 @@ const letterHeight = 2.4;
 const defaultErrorMargin = 0.1;
 
 // meters to pixels
-const visualisationScale = 20;
+const visualisationScale = 10;
 
 // width 
 const letterWidth = {
-  " ": 0.6,
-  "A": 0.8,
-  "B": 0.8,
-  "C": 0.8,
-  "D": 0.8,
-  "E": 0.8,
-  "F": 0.8,
-  "G": 0.8,
-  "H": 0.8,
-  "I": 0.8,
-  "J": 0.8,
-  "K": 0.8,
-  "L": 0.8,
-  "M": 1.2,
-  "N": 0.8,
-  "O": 0.8,
-  "P": 0.8,
-  "Q": 0.8,
-  "R": 0.8,
-  "S": 0.8,
-  "T": 0.8,
-  "U": 0.8,
-  "V": 0.8,
-  "W": 1.2,
-  "X": 0.8,
-  "Y": 0.8,
-  "Z": 0.8,
-  "?": 0.8,
-  ":": 0.8,
-  "*": 0.8,
+  " ": 1.0,
+  "A": 1.485,
+  "B": 1.485,
+  "C": 1.485,
+  "D": 1.485,
+  "E": 1.485,
+  "F": 1.485,
+  "G": 1.485,
+  "H": 1.485,
+  "I": 1.485,
+  "J": 1.485,
+  "K": 1.485,
+  "L": 1.485,
+  "M": 1.885,
+  "N": 1.485,
+  "O": 1.485,
+  "P": 1.485,
+  "Q": 1.485,
+  "R": 1.485,
+  "S": 1.485,
+  "T": 1.485,
+  "U": 1.485,
+  "V": 1.885,
+  "W": 1.685,
+  "X": 1.485,
+  "Y": 1.485,
+  "Z": 1.485,
+  "?": 1.485,
+  ":": 1.485,
+  "*": 1.485,
 }
 
 // additional spacing left and right
@@ -231,7 +231,7 @@ class SpacerPage extends Component {
         textAlign: "center",
         lineHeight: (visualisationScale * letterHeight) + "px",
         fontFamily: 'Impact',
-        fontSize: visualisationScale * letterHeight / 2
+        fontSize: visualisationScale * letterHeight / 1.2
       }}>{l.letter}</span>
     );
     return (
