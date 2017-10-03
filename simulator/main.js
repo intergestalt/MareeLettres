0724cc8e7a636c847dd74e8c4347fcf743d65114
Holger Heißmeyer <holgheiss@gmail.com>
2017-10-03 11:05:24 +0200

    simulator

--- simulator/main.js
+++ simulator/main.js
@@ -1,10 +1,13 @@
 const { OriginId } = require('maree-lettres-shared');
 const request = require('request-promise');
 const Random = require('random-js');
+var Promise = require('bluebird');
 
-const api_prefix = "http://localhost:3333/api/";
-const parallel = 1;
-const continuous = false;
+//const api_prefix = "http://localhost:3333/api/";
+const api_prefix = "https://maree-dev.herokuapp.com/api/";
+const parallel = 5;
+const continuous = true;
+const proposal_probability = 0.01;
 
 const r = Random();
 let counter = 0;
@@ -27,11 +30,6 @@ const run_test = function (callback) {
       const challenges = JSON.parse(data).challenges
       challenge = r.sample(challenges, 1)[0]
       //console.log(challenge._id)
-      // GET tinder proposals
-      request(api_prefix + 'tinder/' + challenge._id + "/" + origin_id + "?limit=100").then(function (proposals) {
-        //console.log(JSON.parse(proposals))
-        console.log("got " + JSON.parse(proposals).proposals.length + " tinder proposals")
-      })
       // GET proposals
       request(api_prefix + 'challenges/' + challenge._id + "/proposals?sort=popular&limit=40").then(function (proposals) {
         //console.log(JSON.parse(proposals))
@@ -41,6 +39,31 @@ const run_test = function (callback) {
       })
       request(api_prefix + 'challenges/' + challenge._id + "/proposals?sort=trending&limit=40").then(function (proposals) {
         //console.log(JSON.parse(proposals))
+      })
+      return data
+    })
+    .then(function (data) {
+      // console.log(data);
+      // pick random challenge
+      const challenges = JSON.parse(data).challenges
+      challenge = r.sample(challenges, 1)[0]
+      //console.log(challenge._id)
+      // GET tinder proposals
+      request(api_prefix + 'tinder/' + challenge._id + "/" + origin_id + "?limit=100").then(function (proposals_res) {
+        //console.log(JSON.parse(proposals))
+        const proposals = JSON.parse(proposals_res).proposals;
+        console.log("got " + proposals.length + " tinder proposals")
+        Promise.each(proposals, function (proposal) {
+          const body = { votes: {} }
+          body.votes[proposal._id] = r.bool(0.6);
+          request.post(api_prefix + 'players/' + origin_id + '/votes').form(body)
+            .then(function (vote_res) {
+              console.log('Voted', vote_res);
+            })
+            .catch(function (e) {
+              console.log(e.message)
+            })
+        })
       })
       return data
     })
@@ -55,14 +78,17 @@ const run_test = function (callback) {
         }
       }
       // console.log(body)
-      return request.post(api_prefix + 'proposals').form(body)
-    })
-    .then(function (data) {
-      console.log("proposed " + JSON.parse(data).proposal._id)
-      // console.log(JSON.parse(data))
+      if (r.bool(proposal_probability)) {
+        request.post(api_prefix + 'proposals').form(body)
+          .then(function (data) {
+            console.log("proposed " + JSON.parse(data).proposal._id)
+            // console.log(JSON.parse(data))
+          })
+      }
+      return data
     })
     .catch(function (e) {
-      console.log(e)
+      console.log(e.message)
     })
     .finally(function (data) {
       console.log("done " + device_string)
@@ -82,3 +108,9 @@ let shuffleString = function (str) {
     })
     .join('');
 };
+
+function delay(t) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, t)
+  });
+}
