8dd3e7d48c6a1725b87757425e4f09cde9423f7c
Sebastian Quack <sebastianquack@gmail.com>
2017-10-05 17:10:47 +0200

    set website to fastly

--- web/maree-web/src/config/config.js
+++ web/maree-web/src/config/config.js
@@ -1,4 +1,5 @@
-const serverUri = "https://maree-dev-herokuapp-com.global.ssl.fastly.net";
+//const serverUri = "https://maree-dev-herokuapp-com.global.ssl.fastly.net";
+const serverUri = "https://api.lettres.paris";
 // const serverUri = "https://mareedeslettres.herokuapp.com";
 //const serverUri = "http://0.0.0.0:3000";
 //const serverUri = "http://maree-dev.herokuapp.com";
